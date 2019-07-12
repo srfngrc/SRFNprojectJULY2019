1e80e840b7edfa49bd15aed1cfc8847382c1d48c
sg <srfngrc@outlook.com>
2019-07-12 16:34:48 -0400

    july12th 4h34pm upload

--- EachODE_Screen.Designer.cs
+++ EachODE_Screen.Designer.cs
@@ -36,6 +36,12 @@
             this.btn_EODE_Cancel = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnLoadDataFromDBcust = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,14 +51,14 @@
             this.lblISIN.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblISIN.Location = new System.Drawing.Point(27, 44);
             this.lblISIN.Name = "lblISIN";
-            this.lblISIN.Size = new System.Drawing.Size(36, 15);
+            this.lblISIN.Size = new System.Drawing.Size(76, 15);
             this.lblISIN.TabIndex = 0;
-            this.lblISIN.Text = "ISIN: ";
+            this.lblISIN.Text = "ISIN Number: ";
             this.lblISIN.Click += new System.EventHandler(this.Label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(93, 44);
+            this.textBox1.Location = new System.Drawing.Point(144, 41);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
@@ -74,13 +80,13 @@
             this.lbl_Date_Buy.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lbl_Date_Buy.Location = new System.Drawing.Point(27, 76);
             this.lbl_Date_Buy.Name = "lbl_Date_Buy";
-            this.lbl_Date_Buy.Size = new System.Drawing.Size(57, 15);
+            this.lbl_Date_Buy.Size = new System.Drawing.Size(84, 15);
             this.lbl_Date_Buy.TabIndex = 3;
-            this.lbl_Date_Buy.Text = "Buy date: ";
+            this.lbl_Date_Buy.Text = "Purchase date: ";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(93, 76);
+            this.dateTimePicker1.Location = new System.Drawing.Point(144, 71);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(188, 20);
             this.dateTimePicker1.TabIndex = 4;
@@ -100,7 +106,7 @@
             // 
             this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(27, 142);
+            this.dataGridView1.Location = new System.Drawing.Point(27, 187);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(336, 180);
             this.dataGridView1.TabIndex = 6;
@@ -108,7 +114,7 @@
             // 
             // btnLoadDataFromDBcust
             // 
-            this.btnLoadDataFromDBcust.Location = new System.Drawing.Point(93, 337);
+            this.btnLoadDataFromDBcust.Location = new System.Drawing.Point(93, 373);
             this.btnLoadDataFromDBcust.Name = "btnLoadDataFromDBcust";
             this.btnLoadDataFromDBcust.Size = new System.Drawing.Size(219, 23);
             this.btnLoadDataFromDBcust.TabIndex = 7;
@@ -116,11 +122,68 @@
             this.btnLoadDataFromDBcust.UseVisualStyleBackColor = true;
             this.btnLoadDataFromDBcust.Click += new System.EventHandler(this.BtnLoadDataFromDBcust_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(27, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Sell date: ";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(144, 97);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(188, 20);
+            this.dateTimePicker2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(27, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Operation Amount: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(27, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Operation Description:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(144, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(144, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(208, 20);
+            this.textBox3.TabIndex = 13;
+            // 
             // EachODE_Screen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(456, 484);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnLoadDataFromDBcust);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btn_EODE_Cancel);
@@ -147,5 +210,11 @@
         private System.Windows.Forms.Button btn_EODE_Cancel;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnLoadDataFromDBcust;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
