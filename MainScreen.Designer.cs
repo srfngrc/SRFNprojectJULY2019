e9ac159596682fddb5e6f47f73402cec067f059e
sg <srfngrc@outlook.com>
2019-07-17 20:47:40 -0400

    last by now. im gonna try in WPF july17 8h47

--- MainScreen.Designer.cs
+++ MainScreen.Designer.cs
@@ -255,7 +255,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(649, 323);
+            this.ClientSize = new System.Drawing.Size(653, 316);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
