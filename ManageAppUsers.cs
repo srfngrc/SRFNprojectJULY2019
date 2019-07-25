b78fad04ad21e0e8a4c38171c51f42d707b0d0fc
sg <srfngrc@outlook.com>
2019-07-24 20:33:08 -0400

    added the form to manage Users. july24th 8h33pm

--- ManageAppUsers.cs
+++ ManageAppUsers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,42 @@ namespace SRFNprojectJULY2019proj
 {
     public partial class ManageAppUsers : Form
     {
+        static string connectionSRFN = "Server=DatabaseSRFN.mssql.somee.com;" +
+            "Database=DatabaseSRFN;" +
+            "User Id=serafin; "+
+            "Password=19771977;";
+        SqlConnection connect123 = new SqlConnection(connectionSRFN);
+        
         public ManageAppUsers()
         {
             InitializeComponent();
+            LoadDataGrid_logins();
+        }
+
+        public void LoadDataGrid_logins()
+        {
+            try
+            {
+                string myquery = "select * from Nutella.logins;";
+                SqlCommand mycommand = new SqlCommand(myquery, connect123);
+                connect123.Open();
+
+                SqlDataReader WhatsRead = mycommand.ExecuteReader();
+                DataTable tabla = new DataTable();
+                tabla.Load(WhatsRead);
+                dataGridView1.DataSource = tabla;
+            }
+            catch( Exception ex2)
+            {
+                MessageBox.Show(ex2.Message);
+            }
+            finally
+            {
+                if (connect123.State == ConnectionState.Open)
+                {
+                    connect123.Close();
+                }
+            }
         }
     }
 }
